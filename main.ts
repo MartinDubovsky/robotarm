d3588a7e68be4b03c0f4e0267fa49541c6ef45d0
MartinDubovsky <107799363+MartinDubovsky@users.noreply.github.com>
2022-10-16 14:31:21 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,47 +6,46 @@ bluetooth.onBluetoothDisconnected(function () {
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
-        wuKong.setMotorSpeed(wuKong.MotorList.M2, 60)
+        pins.servoSetPulse(AnalogPin.P0, left)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
-        wuKong.setMotorSpeed(wuKong.MotorList.M2, -60)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
-        wuKong.setMotorSpeed(wuKong.MotorList.M1, -25)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
-        wuKong.setMotorSpeed(wuKong.MotorList.M1, 25)
+        pins.servoSetPulse(AnalogPin.P0, right)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
-        delta = step
+        wuKong.setMotorSpeed(wuKong.MotorList.M1, 100)
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, -100)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
-        delta = 0 - step
+        wuKong.setMotorSpeed(wuKong.MotorList.M1, -100)
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, 100)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_UP) {
-        delta = 0
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_UP) {
-        delta = 0
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_UP || control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
-        wuKong.setMotorSpeed(wuKong.MotorList.M2, 0)
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_UP || control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_UP) {
         wuKong.setMotorSpeed(wuKong.MotorList.M1, 0)
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, 0)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_UP) {
+        wuKong.setMotorSpeed(wuKong.MotorList.M1, 0)
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, 0)
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_UP || control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
+        pins.servoSetPulse(AnalogPin.P0, center)
     } else {
         wuKong.setMotorSpeed(wuKong.MotorList.M1, 0)
         wuKong.setMotorSpeed(wuKong.MotorList.M2, 0)
     }
 })
-let step = 0
-let delta = 0
+let right = 0
+let left = 0
+let center = 0
 basic.showLeds(`
     . . . . .
     . . . . .
     . . # . .
     . . . . .
-    . . . . .
+    . . # . .
     `)
-let angle = 1500
-delta = 0
-step = 10
-wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angle)
+center = 1540
+left = 1040
+right = 2040
+let delta = 0
+let step = 10
+pins.servoSetPulse(AnalogPin.P0, center)
 wuKong.setMotorSpeed(wuKong.MotorList.M1, 0)
 wuKong.setMotorSpeed(wuKong.MotorList.M2, 0)
 basic.forever(function () {
-    angle = angle + delta
-    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, angle)
-    pins.servoSetPulse(AnalogPin.P0, angle)
+	
 })
